f98c21282363a1c79f132a6dd76cb833bac1ebe8
Joemarcus <joe@whatsgoodapps.com>
2025-05-17 21:50:02 -0400

    Add AiSports project and fix itemFormatter

---
+++ WhoWillWin/Who will win/AiSports/AiSportsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiSportsApp.swift
+//  AiSports
+//
+//  Created by Joe Marcus on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct AiSportsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WhoWillWin/Who will win/AiSportsTests/AiSportsTests.swift
@@ -0,0 +1,17 @@
+//
+//  AiSportsTests.swift
+//  AiSportsTests
+//
+//  Created by Joe Marcus on 2/6/25.
+//
+
+import Testing
+@testable import AiSports
+
+struct AiSportsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
